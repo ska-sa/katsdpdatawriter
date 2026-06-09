e5a0fd25165daf9567466e4c9be110700635b673
Kim McAlpine <kimmcalpine@gmail.com>
2026-06-09 13:19:55 +0200

    Log slice of chunk

--- katsdpdatawriter/spead_write.py
+++ katsdpdatawriter/spead_write.py
@@ -266,7 +266,7 @@ class ChunkStoreRechunker(rechunk.Rechunker):
         self._loop.call_soon_threadsafe(increment_active_chunks)
         try:
             self.chunk_store.put_chunk(self.name, slices, value)
-            logger.info('Putting chunk {0} into chunkstore'.format(self.name))
+            logger.info('Putting chunk {0} slice {1} into chunkstore'.format(self.name, slices))
         finally:
             self._loop.call_soon_threadsafe(decrement_active_chunks)
         end = time.monotonic()
